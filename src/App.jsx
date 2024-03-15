3e3cc15e9e616d86b81800f383de1305ad0cab5c
unknown <kalidashodekare14@gmail.com>
2024-03-15 23:21:10 +0600

    Assignment project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+import Recipes from './Components/Recipes/Recipes';
+import Cart from './Components/Cart/Cart';
 
 function App() {
 
   return (
     <>
-      <header>
+      <header className='mx- lg:mx-20'>
         {/* Navbar */}
         <nav>
-          <div className="navbar bg-base-100">
+          <div className="navbar bg-base-100 lg:m-5">
             <div className="navbar-start">
               <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,25 +29,51 @@ function App() {
                   <li><a>Item 3</a></li>
                 </ul>
               </div>
-              <a className="btn btn-ghost text-xl">Recipe Calories</a>
+              <a className="btn btn-ghost text-2xl font-bold">Recipe Calories</a>
             </div>
             <div className="navbar-center hidden lg:flex">
               <ul className="menu menu-horizontal px-1">
-                <li><a href="">Home</a></li>
-                <li><a href="">Recipes</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Search</a></li>
+                <li><a className='font-[500] text-[15px] text-[#5B546A]' href="">Home</a></li>
+                <li><a className='font-[500] text-[15px] text-[#5B546A]' href="">Recipes</a></li>
+                <li><a className='font-[500] text-[15px] text-[#5B546A]' href="">About</a></li>
+                <li><a className='font-[500] text-[15px] text-[#5B546A]' href="">Search</a></li>
               </ul>
             </div>
-            <div className="navbar-end">
-              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-              <a className="btn">Button</a>
+            <div className="navbar-end space-x-3">
+              <div className='lg:flex justify-center items-center border pl-3 rounded-full hidden'>
+                <IoSearchOutline className='text-2xl' />
+                <input type="text" placeholder="Search" className="input rounded-full  w-60 max-w-xs" />
+              </div>
+              <a className='bg-[#0BE58A] p-2 rounded-full' href="#"><FaRegUserCircle className='text-4xl text-[#000000b7]' /></a>
             </div>
           </div>
         </nav>
+        {/* Banner */}
+        <div>
+          <div className="bg-[url('./assets/Rectangle-1.png')] bg-center h-[80vh] bg-no-repeat rounded-2xl flex flex-col justify-center items-center text-white space-y-4">
+            <h1 className='text-5xl lg:w-[60%] text-center font-semibold'>Discover an exceptional cooking class tailored for you!</h1>
+            <p className='lg:w-[63%] text-center text-[#d9d8d8]'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+            <div className='flex mr-5 gap-3'>
+              <a href="">
+                <button className=' bg-[#0BE58A] border-0 font-bold text-[18px]  rounded-full w-44 h-[10vh] text-[#000000e7]'>Explore Now</button>
+              </a>
+              <a href="">
+                <button className=' text-[18px] font-semibold rounded-full w-44 h-[10vh] border '>Our Feedback</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </header>
-      <main>
-
+      <main className='mx- lg:mx-20'>
+        <div className='flex flex-col justify-center items-center w-[70%] text-center m-auto p-10 space-y-5'>
+          <h1 className='text-4xl font-bold '>Our Recipes</h1>
+          <p className='text-[18px] font-[500] text-[#050404ac]'>A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a foodservice operation.</p>
+        </div>
+        {/* Cards */}
+        <div className='flex justify-between'>
+          <Recipes></Recipes>
+          <Cart></Cart>
+        </div>
       </main>
       <footer>
 
---
+++ src/Components/Recipes/Recipes.jsx
@@ -0,0 +1,195 @@
+import { CiClock2 } from "react-icons/ci";
+import { FaFire } from "react-icons/fa";
+const Recipes = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Recipes</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                <div className="card  bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-semibold">Spaghetti Bolognese</h1>
+                        <p className="text-[16px] font-[500] text-[#000000ab]">Classic Italian pasta dish with savory meat sauce.</p>
+                        <hr />
+                        <p className="text-[22px] font-[500]">Ingredients: 6</p>
+                        <l>
+                            <li className="font-[500] text-[#000000ab]">500g ground beef
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">1 onion, chopped
+                            </li>
+                            <li className="font-[500] text-[#000000ab]">2 cloves garlic, minced</li>
+                        </l>
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-3 items-center">
+                                <CiClock2 />
+                                <p className="font-[400] text-[#000000ab]">30 minutes</p>
+                            </div>
+                            <div className="flex gap-3 items-center">
+                                <FaFire />
+                                <p className="font-[400] text-[#000000ab]">600 calories</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Recipes;
